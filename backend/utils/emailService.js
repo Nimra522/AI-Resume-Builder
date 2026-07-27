3923d56278ffd675ff1fca3d42bcf3d0de3365f3
codewhiznimzy <craftedbynimra@gmail.com>
2026-07-27 08:48:44 +0500

    Forgot password set

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -6,8 +6,17 @@ const nodemailer = require('nodemailer');
 // (Gmail app password: https://myaccount.google.com/apppasswords)
 
 const sendEmail = async ({ to, subject, html }) => {
+
+  console.log("Email Data:", {
+    to,
+    subject,
+    htmlLength: html?.length,
+    html
+  });
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
